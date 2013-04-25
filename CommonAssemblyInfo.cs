0bc6c6b060907370f49f7f08535919fb9d054637
Chris Eargle <kodefuguru@live.com>
2013-04-24 22:05:48 -0400

    Refreshed from code provided on telerik.com

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("2013.1.319.4")]
+[assembly: AssemblyFileVersion("2013.1.319.4")]
---
+++ readme.md
@@ -0,0 +1,3 @@
+*GoToEntryPoint JustDecompile Plugin
+
+This plugin was created by Bernhard Lang and released under MS-PL.
